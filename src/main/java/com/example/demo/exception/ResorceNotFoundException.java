c3cc11acdfcfd14834fec12c16b865563dc300a0
sai teja <avsaiteja94@gmail.com>
2024-07-02 11:31:06 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/exception/ResorceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResorceNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1L;
+
+    public ResorceNotFoundException(String message) {
+        super(message);
+    }
+}
